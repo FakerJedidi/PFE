8c2ac2aa76bf2b69dfdb820e0c02d34e97044eec
Faker JEDIDI <100872873+FakerJedidi@users.noreply.github.com>
2023-02-26 19:26:29 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -3,8 +3,17 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LanguageScreen from "./screens/LanguageScreen";
 import SplashScreen from './screens/SplashScreen';
 import LicenseScreen from "./screens/LicenseScreen";
-
+import FormulaireScreen from "./screens/FormulaireScreen";
+import CodeScreen from "./screens/CodeScreen";
 const Stack = createNativeStackNavigator();
+import PercodeScreen from "./screens/PercodeScreen";
+import pointageScreen from "./screens/pointageScreen";
+import ArriveeScreen from "./screens/ArriveeScrenn";
+import DepartScreen from "./screens/DepartScreen";
+import PauseScreen from "./screens/PauseScreen";
+import RetpauseScreen from "./screens/RetpauseScreen";
+
+
 
 export default function App() {
   return (
@@ -25,6 +34,48 @@ export default function App() {
           component={LicenseScreen}
           options={{title: 'Vérify License'}}
         />
+        <Stack.Screen 
+          name="Formulaire"
+          component={FormulaireScreen}
+          options={{title: 'Form'}}
+        />
+        <Stack.Screen 
+          name="Code"
+          component={CodeScreen}
+          options={{title: 'code'}}
+        />
+          <Stack.Screen 
+          name="Percode"
+          component={PercodeScreen}
+          options={{title: 'Percode'}}
+        />
+          <Stack.Screen 
+          name="pointage"
+          component={pointageScreen}
+          options={{title: 'pointage'}}
+        />
+
+          <Stack.Screen 
+          name="Arrivee"
+          component={ArriveeScreen}
+          options={{title: 'Arrivee'}}
+        />
+          <Stack.Screen 
+          name="Depart"
+          component={DepartScreen}
+          options={{title: 'Depart'}}
+        />
+           <Stack.Screen 
+          name="Pause"
+          component={PauseScreen}
+          options={{title: 'Pause'}}
+        />
+         <Stack.Screen 
+          name="Retpause"
+          component={RetpauseScreen}
+          options={{title: 'Retpause'}}
+        />
+        
       </Stack.Navigator>
     </NavigationContainer>
 
