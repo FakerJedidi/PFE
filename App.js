499f337ca9bda67ff7ecdd0da2dcef7f72bc7b80
Faker JEDIDI <100872873+FakerJedidi@users.noreply.github.com>
2023-03-30 23:01:39 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,85 +1,23 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LanguageScreen from "./screens/LanguageScreen";
-import SplashScreen from './screens/SplashScreen';
-import LicenseScreen from "./screens/LicenseScreen";
-import FormulaireScreen from "./screens/FormulaireScreen";
-import CodeScreen from "./screens/CodeScreen";
-const Stack = createNativeStackNavigator();
-import PercodeScreen from "./screens/PercodeScreen";
-import pointageScreen from "./screens/pointageScreen";
-import ArriveeScreen from "./screens/ArriveeScrenn";
-import DepartScreen from "./screens/DepartScreen";
-import PauseScreen from "./screens/PauseScreen";
-import RetpauseScreen from "./screens/RetpauseScreen";
-
-
-
+import Navigation from "./src/navigation/navigation";
+import { StyleSheet, SafeAreaView } from "react-native";
+import { Provider, useSelector } from 'react-redux';
+import { persistor, store } from './src/store/rootReducer'
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="Splash"
-          component={SplashScreen}
-          options={{title: 'Welcome'}}
-        />
-        <Stack.Screen 
-          name="Language"
-          component={LanguageScreen}
-          options={{title: 'Language choice'}}
-        />
-        <Stack.Screen 
-          name="License"
-          component={LicenseScreen}
-          options={{title: 'Vérify License'}}
-        />
-        <Stack.Screen 
-          name="Formulaire"
-          component={FormulaireScreen}
-          options={{title: 'Form'}}
-        />
-        <Stack.Screen 
-          name="Code"
-          component={CodeScreen}
-          options={{title: 'code'}}
-        />
-          <Stack.Screen 
-          name="Percode"
-          component={PercodeScreen}
-          options={{title: 'Percode'}}
-        />
-          <Stack.Screen 
-          name="pointage"
-          component={pointageScreen}
-          options={{title: 'pointage'}}
-        />
 
-          <Stack.Screen 
-          name="Arrivee"
-          component={ArriveeScreen}
-          options={{title: 'Arrivee'}}
-        />
-          <Stack.Screen 
-          name="Depart"
-          component={DepartScreen}
-          options={{title: 'Depart'}}
-        />
-           <Stack.Screen 
-          name="Pause"
-          component={PauseScreen}
-          options={{title: 'Pause'}}
-        />
-         <Stack.Screen 
-          name="Retpause"
-          component={RetpauseScreen}
-          options={{title: 'Retpause'}}
-        />
-        
-      </Stack.Navigator>
-    </NavigationContainer>
+  return (
+
+    <SafeAreaView style={styles.root}>
+   <Provider store={store}>
+    <Navigation />
+   </Provider>
+    </SafeAreaView>
 
   );
-}
+};
 
-
+const styles = StyleSheet.create({
+    root: {
+      flex: 1,
+      backgroundColor: '#F9FBFC', 
+    },
+});
---
+++ index.js
@@ -0,0 +1,4 @@
+import * as RNLocalize from 'react-native-localize';
+import i18next from "./src/longuages/i18n";
+import {useTranslation} from 'react-native-i18next';
+import { initReactI18next } from "react-i18next";
